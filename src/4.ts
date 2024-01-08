d0f5f97b82c07756989da45c1df87f8785a1ef43
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2024-01-08 15:26:38 +0200

    дз

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,54 @@
+class Key {
+    private signature: number;
+    constructor() {
+        this.signature = Math.random() 
+    }
+    getSignature(): number{
+return this.signature
+}
+
+
+}
+class Person{
+    private key: Key;
+    constructor(key: Key) { this.key = key }
+    getKey(): Key{return this.key}
+
+}
+
+abstract class House{
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = []
+    constructor(key: Key) { this.key = key }
+    
+    abstract openDoor(key: Key): void
+    
+    comeIn(person: Person): void{
+        if (this.door) {
+            this.tenants.push(person);
+            console.log('Привіт дім')
+        }
+        else {
+            console.log('Будинок заблоковано')
+        }
+    }
+
+}
+class MyHouse extends House{
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door == true;
+            console.log('Двері відчинено')
+        }
+        else {
+            console.log('Invalid key. Двері залишаються зачинені!')
+        }
+    }
+}
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
